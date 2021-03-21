dc897c2169893a07cf5b2359bd8b4a96a766b8b0
WoWD23 <xym51002125@gmail.com>
2021-03-21 15:33:07 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 熊毓敏 on 2021/3/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
